eeabc7a381d55c3402006689831b25ed76d97b07
Takashi Ogishima <t.ogi.12112gmail.com>
2023-10-25 21:55:22 +0900

    サブルーチンへ切り出し

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@ fn main() {
     println!("Hello, {}", String::from(str_slice));
 
     for i in 1..16 {
-        if i % 15 == 0 {
-            println!("FizzBuss");
-        } else if i % 5 == 0 {
-            println!("Buzz");
-        } else if i % 3 == 0 {
-            println!("Fizz");
-        } else {
-            println!("{}", i);
-        }
+        println!("{}", toFizzBuzzString(i));
+    }
+}
+
+fn toFizzBuzzString(number: i32) -> String {
+    if number % 15 == 0 {
+        return String::from("FizzBuss");
+    } else if number % 5 == 0 {
+        return String::from("Buzz");
+    } else if number % 3 == 0 {
+        return String::from("Fizz");
+    } else {
+        return format!("{}", number);
     }
 }
