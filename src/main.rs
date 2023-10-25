d7f1080a6768721c77a46d598be8617615517f39
Takashi Ogishima <t.ogi.12112gmail.com>
2023-10-25 21:42:25 +0900

    とりあえずFizzBuzzでifとループの動作確認

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     let str_slice = "World";
     println!("Hello, {}", String::from(str_slice));
+
+    for i in 1..16 {
+        if i % 15 == 0 {
+            println!("FizzBuss");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else {
+            println!("{}", i);
+        }
+    }
 }
